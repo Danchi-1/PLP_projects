c38f072b4343bf7626798472ae9b2d16c89324a6
Danchi-1 <ohachordaniel2021@gmail.com>
2025-05-04 08:05:50 +0100

    Completed we dev assignment for PLP week 5

---
+++ webdev_week5/script.js
@@ -0,0 +1,37 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const greetings = document.getElementById("greet");
+    const nameSpan = document.getElementById("name");
+
+    document.getElementById("background-color").addEventListener("click", function () {
+        if (document.body.style.backgroundColor == "black") {
+            document.body.style.backgroundColor = "white";
+            document.getElementById("background-color").innerHTML = "Black background";
+            document.getElementById("background-color").style.border = "2px solid white"
+            document.getElementById("hide").style.border = "2px solid white"
+        } else {
+            document.body.style.backgroundColor = "black";
+            document.getElementById("background-color").innerHTML = "White background";
+            document.getElementById("background-color").style.border = "2px solid black"
+            document.getElementById("hide").style.border = "2px solid black"
+        }
+    });
+
+    const name = prompt("Please enter your name:");
+    if (name && name.trim() !== "") {
+        nameSpan.innerHTML = name;
+    } else {
+            nameSpan.innerHTML = "Guest";
+    }
+
+    const gibberish = document.getElementById("jargons");
+    document.getElementById("hide").addEventListener("click", function () {
+        if (gibberish.style.display == "none"){
+            gibberish.style.display = "block";
+            document.getElementById("hide").innerHTML = "Hide gibberish";
+        } else {
+            gibberish.style.display = "none";
+            document.getElementById("hide").innerHTML = "Show gibberish";
+
+        }
+    })
+});
